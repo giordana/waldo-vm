65f29040d8cc19a2bf45a38e9b086c4a274315c4
Thiago Prado <tprado@thoughtworks.com>
2014-08-25 17:40:20 -0300

    Provisions Ruby Gems (bundle install).

--- chef/cookbooks/ruby/recipes/default.rb
+++ chef/cookbooks/ruby/recipes/default.rb
@@ -1,12 +1,17 @@
-package 'ruby1.9.3'
-package 'make'
+package 'libsqlite3-dev'
 package 'libxml2-dev'
 package 'libxslt1-dev'
-package 'libsqlite3-dev'
+package 'make'
+package 'ruby1.9.3'
 
 execute 'install bundler' do
   command 'gem install bundler && touch /home/vagrant/bundler-installed'
   user 'root'
   creates '/home/vagrant/bundler-installed'
-  action :run
+end
+
+execute 'install gems' do
+  command 'bundle install'
+  cwd '/vagrant'
+  user 'root'
 end
