ddbe1ebced3e187cdfe52b8c6c298a4fc16e89d6
Thiago Prado <tprado@thoughtworks.com>
2015-04-29 14:32:12 -0300

    Sets Ruby Webrick configuration "DoNotReverseLookup" to true.

--- chef/cookbooks/ruby/recipes/default.rb
+++ chef/cookbooks/ruby/recipes/default.rb
@@ -4,8 +4,12 @@ package 'libxslt1-dev'
 package 'make'
 package 'ruby1.9.3'
 
+cookbook_file 'webrick-config.rb' do
+  path '/usr/lib/ruby/1.9.1/webrick/config.rb'
+  mode '644'
+end
+
 execute 'install bundler' do
   command 'gem install bundler && touch /home/vagrant/bundler-installed'
-  user 'root'
   creates '/home/vagrant/bundler-installed'
 end
